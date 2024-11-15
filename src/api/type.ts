b0c1aacae396bc12e313489d3243b0cf71d7610e
DhruvWebDev <artificialintelligencehub35@gmail.com>
2024-11-15 16:23:43 +0530

    Job Portal v1

--- src/api/type.ts
+++ src/api/type.ts
@@ -1,8 +1,27 @@
- export interface Coordintes {
+ export interface Coordinates {
     lat: number, 
     lon: number
 }
-
 export interface WeatherData {
-   
-}
+    coord: Coordinates;
+    weather: WeatherCondition[];
+    main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+    };
+    wind: {
+      speed: number;
+      deg: number;
+    };
+    city: {
+      sunrise: number;
+      sunset: number;
+      country: string;
+    };
+    name: string;
+    dt: number;
+  }
